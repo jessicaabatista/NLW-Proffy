d628a4a7adaa6dd906b4c8fd464343e29ad23b81
Jéssica Batista <jessicaabta@gmail.com>
2020-08-04 00:10:02 -0300

    add header

---
+++ src/pages/TheacherList/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PageHeader from '../../components/PageHeader';
+
+import './styles.css';
+
+function TheacherList(){
+  return (
+    <div id="page-theacher-list" className="container">
+      <PageHeader title="Estes são os proffys disponíveis." />
+    </div>
+  );
+}
+
+export default TheacherList;
