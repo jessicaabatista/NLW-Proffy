530d8d92e557f3187109778c6d8fd66e2ddedf4a
Jéssica Batista <jessicaabta@gmail.com>
2020-08-04 23:48:56 -0300

    Final da aula 02 - finalização do server
    falta concertar o erro "UnhandledPromiseRejectionWarning: TypeError: time.split is not a function"

--- serverNLW/src/routes.ts
+++ serverNLW/src/routes.ts
@@ -1,34 +1,15 @@
 import express from 'express';
-import db from './database/connection';
+import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
 
 const routes = express.Router();
+const classesControllers = new ClassesController();
+const connectionsController = new ConnectionsController();
 
-routes.post('/classes', async (request, response) => {
-  const {
-    name,
-    avatar,
-    whatsapp,
-    bio,
-    subject,
-    cost,
-    schedule
-  } = request.body;
+routes.get('/classes', classesControllers.index);
+routes.post('/classes', classesControllers.create);
 
-  const insertedUsersIds = await db('users').insert({
-    name,
-    avatar,
-    whatsapp,
-    bio,
-  });
-
-  const user_id = insertedUsersIds[0];
-
-  await db('classes').insert({
-    subject,
-    cost,
-  });
-  
-  return response.send();
-});
+routes.get('/connections', connectionsController.index);
+routes.post('/connections', connectionsController.create);
 
 export default routes;
